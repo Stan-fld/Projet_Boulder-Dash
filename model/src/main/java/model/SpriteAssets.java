73ed1060c948f2712d007819f4cc33f4e62e5673
Kevin ANDRIEUX <kevin.andrieux@viacesi.fr>
2019-05-27 10:07:27 +0200

    Correction SpriteAssets

--- model/src/main/java/model/SpriteAssets.java
+++ model/src/main/java/model/SpriteAssets.java
@@ -10,31 +10,25 @@ import java.io.IOException;
 public class SpriteAssets {
 
         public static final int largeur=16,hauteur=16;
-        BufferedImage wall, dirt, stone, diamond, teleport;
-        BufferedImage img;
+        private BufferedImage img;
         {
             try {
                 img = ImageIO.read(new File("C:\\Users\\vzonz\\OneDrive\\Documents\\Cours\\A1\\Projet\\Projet UML Java 2\\Sprites\\74359.png"));
-                System.out.println("je suis la");
             } catch (IOException e) {
                 System.out.println("le fichier n'existe pas");
             }
         }
-        DecoupageSprite sprite = new DecoupageSprite(img);
-        public void init()
-        {
-            this.wall = sprite.Decoupage(0,0, largeur,hauteur);
-            this.dirt = sprite.Decoupage(largeur,0,largeur,hauteur);
-            this.stone = sprite.Decoupage(3*largeur,0,largeur,hauteur);
-            this.diamond = sprite.Decoupage(4*largeur, 0 , largeur, hauteur);
-            this.teleport = sprite.Decoupage(6*largeur, 0 , largeur, hauteur);
+
+        public void init() {
+
         }
 
-        public BufferedImage getDiamond() {
-            return this.diamond;
-        }
-
+    public BufferedImage Decoupage(int x, int y, int nRow, int nCol)
+    {
+        return img.getSubimage(x,y,nRow * 16,nCol * 16);
     }
+
+}
 /* public class DecoupageSprite
 {
     private BufferedImage image;
@@ -42,10 +36,7 @@ public class SpriteAssets {
     {
         this.image=image;
     }
-    public BufferedImage Decoupage(int x, int y, int largeur, int hauteur)
-    {
-        return image.getSubimage(x,y,largeur,hauteur);
-    }
+
 }
 
 
