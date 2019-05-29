ef75209a3561a489183fbc217c71b28941424880
Valentin <v.zonzon@live.fr>
2019-05-29 15:37:54 +0200

    Correction mouvement et block vide (background block)

--- entity/src/main/java/entity/Level.java
+++ entity/src/main/java/entity/Level.java
@@ -140,7 +140,7 @@ public class Level {
             {
                 blockAUpdate = map[x][y];
                 if (blockAUpdate instanceof Rock && y < tailleMapY - 1 && !alreadyMoved[x][y]) {
-                    if (map[x][y + 1] == null) {
+                    if (map[x][y + 1] instanceof BackgroundDirt) {
                         moveBlock(Direction.DOWN, x, y);
                         alreadyMoved[x][y + 1] = true;
                     }
