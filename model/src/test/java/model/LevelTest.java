bd37e168897b0ae0e3fb52065410bfea939811c3
Stan-fld <49478579+Stan-fld@users.noreply.github.com>
2019-06-03 15:58:19 +0200

    Junit Test in Model Package

---
+++ model/src/test/java/model/LevelTest.java
@@ -0,0 +1,67 @@
+package model;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class LevelTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void getTailleMapX() {
+    }
+
+    @Test
+    public void getTailleMapY() {
+    }
+
+    @Test
+    public void setChanged() {
+    }
+
+    @Test
+    public void getPlayerX() {
+    }
+
+    @Test
+    public void setPlayerX() {
+    }
+
+    @Test
+    public void getPlayerY() {
+    }
+
+    @Test
+    public void setPlayerY() {
+    }
+
+    @Test
+    public void setPlayerPos() {
+    }
+
+    @Test
+    public void moveBlock() {
+    }
+
+    @Test
+    public void getBlock() {
+    }
+
+    @Test
+    public void print() {
+    }
+
+    @Test
+    public void getObservable() {
+    }
+}
