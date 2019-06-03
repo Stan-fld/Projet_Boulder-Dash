da9d940d9a462aee3cdf47fcbf0b24a6db3d7b9a
Valentin <v.zonzon@live.fr>
2019-06-03 12:03:33 +0200

    JavaDoc score class

--- entity/src/main/java/entity/Score.java
+++ entity/src/main/java/entity/Score.java
@@ -3,26 +3,50 @@ package entity;
 
 import java.awt.*;
 
+/**
+ * Score class
+ *
+ * @author Valentin
+ */
 public class Score extends Rectangle {
     public int getScore() {
         return score;
     }
 
+    /**
+     * setScore
+     * @param score
+     */
     public void setScore(int score) {
         this.score = score;
     }
 
+    /**
+     * Score
+     */
     private int score;
 
+    /**
+     * Score constructor
+     */
     public Score() {
         super(5,5,64, 32);
 
     }
 
+    /**
+     * incrementScore method
+     *
+     */
     public void incrementScore() {
         this.score++;
     }
 
+    /**
+     * print method
+     *
+     * @param g
+     */
     public void print(Graphics g) {
         g.setColor(Color.white);
         g.fillRoundRect((int) this.getX(), (int) this.getY(), (int) this.getWidth(), (int) this.getHeight(), 5, 5);
